917fc6a5dabe98eeab51afd715574c2937a9eee1
utter-step <8uk.8ak@gmail.com>
2013-06-06 01:12:51 +0400

    Started writing Image class
    Currently performance is awful, MUST fix later.

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using VStepanov.Experiments.Vinyl.Imaging;
+
 namespace Test
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var image = Image.FromFile("../../../TEST_DATA/04.png");
+
+            Console.WriteLine(image[900, 140]);
         }
     }
 }
--- VStepanov.Experiments.Vinyl.Imaging/Image.cs
+++ VStepanov.Experiments.Vinyl.Imaging/Image.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.IO;
 
 namespace VStepanov.Experiments.Vinyl.Imaging
 {
     public class Image
     {
+        private byte[,] _imageData;
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        /// <summary>
+        /// Class, providing access to image data.
+        /// 
+        /// Assuming that image is grayscale.
+        /// </summary>
+        /// <param name="bitmap">Source image.</param>
+        public Image(Bitmap bitmap)
+        {
+            Width  = bitmap.Width;
+            Height = bitmap.Height;
+
+            _imageData = new byte[Width, Height];
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    _imageData[x, y] = bitmap.GetPixel(x, y).R;
+                }
+            }
+        }
+
+        public static Image FromFile(string path)
+        {
+            var bitmap = new Bitmap(path);
+
+            return new Image(bitmap);
+        }
+
+        /// <summary>
+        /// Returns lightness of desired pixel.
+        /// </summary>
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <returns>Lightness at [x, y]</returns>
+        public byte this[int x, int y]
+        {
+            get
+            {
+                return _imageData[x, y]; 
+            }
+        }
     }
 }
